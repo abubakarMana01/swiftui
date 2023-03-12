5591c1c58144d35833b9f5666f0e7944a08e7382
abubakarMana01 <ym.abubakr7@gmail.com>
2023-03-12 16:55:55 +0100

    Initial Commit

---
+++ swiftui-tut/swiftui_tutApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tutApp.swift
+//  swiftui-tut
+//
+//  Created by Abubakar Mana on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
